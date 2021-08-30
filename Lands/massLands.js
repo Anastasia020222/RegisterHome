4e26bac4ff744b81d9cd956c3a5bced71df4d6ec
Anastasia020222 <stasyyyaa@mail.ru>
2021-08-30 22:36:42 +0300

    new lands

--- Lands/massLands.js
+++ Lands/massLands.js
@@ -24,12 +24,26 @@ const massLands = [
     ['GA-FX-LPL65-04-01', {'deeplink_action': 'trading', 'project': 'fx'}],
     ['GA-FX-LPL65-03-02', {'deeplink_action': 'trading', 'project': 'fx'}],
     ['GA-FX-LPL65-03-03', {'deeplink_action': 'trading', 'project': 'fx'}],
+    ['FX-LPL76-01-01', {'asset': 'Bitcoin', 'deeplink_action': 'trading', 'project': 'fx'}],
+    ['FX-LPL76-01-02', {'asset': 'GBPUSD', 'deeplink_action': 'trading', 'project': 'fx'}],
+    ['FX-LPL76-01-03', {'asset': 'EURUSD', 'deeplink_action': 'trading', 'project': 'fx'}],
+    ['FX-LPL76-01-04', {'asset': 'AAPL', 'deeplink_action': 'trading', 'project': 'fx'}],
+    ['FX-LPL76-01-05', {'asset': 'NQ', 'deeplink_action': 'trading', 'project': 'fx'}],
+    ['FX-LPL76-01-06', {'asset': 'ES', 'deeplink_action': 'trading', 'project': 'fx'}],
+    ['FX-LPL76-01-07', {'asset': 'TSLA', 'deeplink_action': 'trading', 'project': 'fx'}],
+    ['FX-LPL76-01-08', {'asset': 'XAUUSD', 'deeplink_action': 'trading', 'project': 'fx'}],
+    ['FX-LPL76-01-09', {'asset': 'YM', 'deeplink_action': 'trading', 'project': 'fx'}],
+    ['FX-LPL76-01-10', {'asset': 'FDAX', 'deeplink_action': 'trading', 'project': 'fx'}],
+    ['FX-LPL76-01-11', {'asset': 'AAP', 'deeplink_action': 'trading', 'project': 'fx'}],
+    ['FX-LPL76-01-12', {'asset': 'AAPL', 'deeplink_action': 'tradin', 'project': 'fx'}]
+
 ];  
 
 //ленды с карточками активов на странице
 
 const massLandsCards = [
     ['FX-LPL76-01-11', {}]
+    ['FX-LPL76-01-12']
 ];
 
 exports.massLandsCards = massLandsCards;
